c3d1a82bfa3327c82bd4bc1b89e71f576628089c
Coleman <33269438+E-Cole-I@users.noreply.github.com>
2020-05-14 12:27:06 -0400

    Add files via upload

---
+++ TweetPull/jsonconfig.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Database Information pulled from external file
+def json_config(file_path):
+    import json
+    with open(file_path) as json_data_file:
+        data = json.load(json_data_file)
+    return data
+
